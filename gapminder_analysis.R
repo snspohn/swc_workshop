0da0566e7ac79db8567a7e5136b6a0292adba066
Stephanie N Spohn <spohn.stephanie@gmail.com>
2017-01-18 10:42:13 -0500

    Adding gapminder_analysis.R to repository

---
+++ gapminder_analysis.R
@@ -0,0 +1,13 @@
+
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
+gapminder <- read.csv("gapminder-FiveYearData.csv")
+# pat likes to look at head, str. colnames, summary, nrow, ncol, dim, View
+# this is part of "data hygiene", this is a useful way of looking to
+#make sure your data is in shape to actually be played with
+
+#life expectancies in Africa for 2007
+#problem to solve: african country life expectancies for 2007
+
+ggplot(data=gapminder, aes(s=gdpPercap, y=lifeExp))+geom_point()
+
+#make life expectancy and gdp chart, color by continent and size by pop
